b6d0182786372a85c7c834fea5435f214069cb4d
Douglas Brion <me@douglasbrion.com>
2019-12-19 23:35:18 +0000

    Updating docstrong for lenet

--- lenet.py
+++ lenet.py
@@ -3,19 +3,6 @@ from collections import OrderedDict
 
 
 class LeNet5(nn.Module):
-    """
-    Input - 1x32x32
-    C1 - 6@28x28 (5x5 kernel)
-    tanh
-    S2 - 6@14x14 (2x2 kernel, stride 2) Subsampling
-    C3 - 16@10x10 (5x5 kernel, complicated shit)
-    tanh
-    S4 - 16@5x5 (2x2 kernel, stride 2) Subsampling
-    C5 - 120@1x1 (5x5 kernel)
-    F6 - 84
-    tanh
-    F7 - 10 (Output)
-    """
 
     def __init__(self):
         super().__init__()

f6cef19ecafffc873929a2d7651102a18badaa52
Douglas Brion <me@douglasbrion.com>
2019-12-19 17:29:36 +0000

    MNIST dataloaders

---
+++ main.py
@@ -0,0 +1,23 @@
+from lenet import LeNet5
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.datasets.mnist import MNIST
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+data_train = MNIST('./data/mnist',
+                   download=True,
+                   transform=transforms.Compose([
+                       transforms.Resize((32, 32)),
+                       transforms.ToTensor()]))
+data_test = MNIST('./data/mnist',
+                  train=False,
+                  download=True,
+                  transform=transforms.Compose([
+                      transforms.Resize((32, 32)),
+                      transforms.ToTensor()]))
+dataloader_train = DataLoader(
+    data_train, batch_size=256, shuffle=True, num_workers=8)
+dataloader_test = DataLoader(
+    data_test, batch_size=1024, num_workers=8)

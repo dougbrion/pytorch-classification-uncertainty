c53abedcfd95602958ab6ca2bb347c522d574261
Douglas Brion <me@douglasbrion.com>
2019-12-29 15:41:34 +0000

    Loglikelihood loss and starting KL div

--- losses.py
+++ losses.py
@@ -7,14 +7,49 @@ def relu_evidence(y):
     return F.relu(y)
 
 
+def exp_evidence(y):
+    return torch.exp(torch.clamp(y, -10, 10))
+
+
+def softplus_evidence(y):
+    return F.softplus(y)
+
+
 def get_device():
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda:0" if use_cuda else "cpu")
     return device
 
 
-def mse_loss():
+def kl_divergence(alpha, num_classes, device=None):
     pass
+
+
+def loglikelihood_loss(y, alpha, device=None):
+    if not device:
+        device = get_device()
+    S = torch.sum(alpha, dim=1, keepdim=True)
+    loglikelihood_err = torch.sum(
+        (y - (alpha / S)) ** 2, dim=1, keepdim=True)
+    loglikelihood_var = torch.sum(
+        alpha * (S - alpha) / (S * S * (S + 1)), dim=1, keepdim=True)
+    loglikelihood = loglikelihood_err + loglikelihood_var
+    return loglikelihood
+
+
+def mse_loss(y, alpha, epoch_num, num_classes, annealing_step, device=None):
+    if not device:
+        device = get_device()
+
+    loglikelihood = loglikelihood_loss(y, alpha, device=device)
+
+    annealing_coef = torch.min(torch.tensor(
+        1.0, dtype=torch.float32), torch.tensor(epoch_num / annealing_step, dtype=torch.float32))
+
+    kl_alpha = (alpha - 1) * (1 - y) + 1
+    kl_div = annealing_coef * \
+        kl_divergence(kl_alpha, num_classes, device=device)
+    return loglikelihood + kl_div
 
 
 def uncertainty_loss(output, target, epoch_num, num_classes, annealing_step, device=None):

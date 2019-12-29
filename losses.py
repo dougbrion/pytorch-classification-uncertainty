438190faf64c944aba8c2f85fd8fc38cc6a87947
Douglas Brion <me@douglasbrion.com>
2019-12-29 15:43:08 +0000

    Adding KL divergence to loss term

--- losses.py
+++ losses.py
@@ -22,7 +22,22 @@ def get_device():
 
 
 def kl_divergence(alpha, num_classes, device=None):
-    pass
+    if not device:
+        device = get_device()
+    beta = torch.ones([1, num_classes], dtype=torch.float32, device=device)
+    S_alpha = torch.sum(alpha, dim=1, keepdim=True)
+    S_beta = torch.sum(beta, dim=1, keepdim=True)
+    lnB = torch.lgamma(S_alpha) - \
+        torch.sum(torch.lgamma(alpha), dim=1, keepdim=True)
+    lnB_uni = torch.sum(torch.lgamma(beta), dim=1,
+                        keepdim=True) - torch.lgamma(S_beta)
+
+    dg0 = torch.digamma(S_alpha)
+    dg1 = torch.digamma(alpha)
+
+    kl = torch.sum((alpha - beta) * (dg1 - dg0), dim=1,
+                   keepdim=True) + lnB + lnB_uni
+    return kl
 
 
 def loglikelihood_loss(y, alpha, device=None):
